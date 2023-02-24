85a4af3fde508d8476e6dc07c91ca531e48472a8
Jxstxs <juleb0202@gmail.com>
2023-02-24 01:49:23 +0100

    feat: added wrapper function for tmultitype creation

--- src/utils/data_types.pas
+++ src/utils/data_types.pas
@@ -9,13 +9,29 @@ Uses
 SysUtils, fgl;
 
 Type 
+    mtypes =   (s, dt, ars);
+
     TMultiType =   Record
         s:   String;
         dt:   TDateTime;
         ars:   array Of String;
     End;
+
     TDict =   specialize TFPGMap<String, TMultiType>;
 
+Function MT(_t: mtypes; _v: variant):   TMultiType;
+
 Implementation
+
+Function MT(_t: mtypes; _v: variant):   TMultiType;
+
+Var 
+    _mt:   TMultiType;
+Begin
+    Case _t Of 
+        s:   _mt.s := _v;
+    End;
+    Result := _mt;
+End;
 
 End.
