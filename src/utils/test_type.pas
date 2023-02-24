9ab91b0d19081e8973ccf3f1f64c14ac4ea27bfd
Jxstxs <juleb0202@gmail.com>
2023-02-24 01:49:42 +0100

    feat: added base ttest structure

---
+++ src/utils/test_type.pas
@@ -0,0 +1,19 @@
+
+Unit test_type;
+
+{$mode ObjFPC}{$H+}
+
+Interface
+
+Uses 
+Classes, SysUtils, base_class;
+
+Type 
+    TTest =   Record
+        linked_notes:   array Of TBase;
+        questions:   array Of TBase;
+    End;
+
+Implementation
+
+End.
