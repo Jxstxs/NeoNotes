4ca5a56d9860ec0279a6a1245f1663b1eca96dfc
Jxstxs <juleb0202@gmail.com>
2023-03-11 14:36:16 +0100

    feat: improved db manager

--- src/classes/db_manager.pas
+++ src/classes/db_manager.pas
@@ -9,27 +9,68 @@ Uses
 SysUtils, Classes, sqldb, sqlite3conn;
 
 Type 
+    // Record to return the current connection and transaction
+    rDb = Record
+        connection: TSQLite3Connection;
+        transaction: TSQLTransaction;
+    end;
+
     cDatabaseManager = Class
     private
-        already_setup: Boolean;
         connection:   TSQLite3Connection;
         transaction:   TSQLTransaction;
     public
         constructor Create();
+        destructor Destroy();
+
+        // Initializes a new Database at db_path
+        procedure setupDb(db_path: String);
+        // Connects to an existing Database at db_path
         procedure linkToFile(db_path: String);
+        // Commits changes made to the current Database
+        procedure commit();
+
+        // Returns the current connection and transaction
+        function getDb(): rDb;
     end;
 
 Implementation
 
 Uses
-    Forms, data_types, start_form, error_form;
+    Forms, data_types, query_class, start_form, error_form;
 
 constructor cDatabaseManager.Create();
 begin
+    inherited Create;
     connection := TSQLite3Connection.Create(Nil);
     transaction := TSQLTransaction.Create(connection);
     connection.Transaction := transaction;
-    already_setup := True;
+end;
+
+destructor cDatabaseManager.Destroy();
+begin
+    inherited Destroy;
+    connection.Free;
+    transaction.Free;
+end;
+
+procedure cDatabaseManager.setupDb(db_path: String);
+Var
+    err_str: String;
+    query: TSQLScript;
+begin
+    connection.DatabaseName := db_path;
+    connection.Open;
+
+    // Creates the needed Tables
+    query := TSQLScript.Create(nil);
+    query.DataBase := connection;
+    query.Transaction := transaction;
+    query.Script.Text := 'CREATE TABLE IF NOT EXISTS dave(id int, stalb text);';
+    query.Execute;
+
+    commit();
+    query.Free;
 end;
 
 procedure cDatabaseManager.linkToFile(db_path: String);
@@ -49,5 +90,19 @@ Begin
         Application.CreateForm(TF_start, F_start);
     End;
 End;
+
+procedure cDatabaseManager.commit();
+begin
+    transaction.Commit;
+end;
+
+function cDatabaseManager.getDb(): rDb;
+Var
+    db: rDb;
+begin
+    db.connection := connection;
+    db.transaction := transaction;
+    Result := db;
+end;
 
 End.
